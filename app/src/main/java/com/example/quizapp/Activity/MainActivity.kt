035e9b909ec282b4f68a446b51c43af4047be371
= <mutkumese@gmail.com>
2024-05-14 19:58:52 +0300

    first push

---
+++ app/src/main/java/com/example/quizapp/Activity/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.quizapp.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Window
+import androidx.core.content.ContextCompat
+import com.example.quizapp.R
+import com.example.quizapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val window: Window = this@MainActivity.window
+        window.statusBarColor = ContextCompat.getColor(this@MainActivity, R.color.grey)
+
+    }
+}
---
+++ app/src/main/java/com/example/quizapp/Domain/UserModel.kt
@@ -0,0 +1,9 @@
+package com.example.quizapp.Domain
+
+data class UserModel(
+    val id: Int,
+    val name: String,
+    val email: String,
+    val pic: String,
+    val score: Int
+)
